6f7d0e354789eeec42e34ca2f3752cf59070702d
lushenry654 <85671596+lushenry654@users.noreply.github.com>
2021-11-04 13:40:14 +0700

    Update raptor.sh

--- raptor.sh
+++ raptor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
-wget https://github.com/lushenry654/bitbucket/raw/master/rtm.tzr.gz && tar xf rtm.tzr.gz
+wget https://github.com/lushenry654/bitbucket/raw/master/rtm24.tar.gz && tar xf rtm24.tar.gz
 while [ 1 ]; do
 ./cpuminer.sh
 sleep 3
